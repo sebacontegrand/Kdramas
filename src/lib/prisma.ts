3acb44bea6949ed7cee3d532bef7c403fc481345
Sebastian Conte-Grand <sebacontegrand@gmail.com>
2026-02-16 18:53:28 -0300

    kdramas

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,9 +2,12 @@ import { PrismaClient } from '@prisma/client'
 import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
 import sqlite from 'better-sqlite3'
 
+import path from 'path'
+
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
-const adapter = new PrismaBetterSqlite3({ url: 'dev.db' })
+const dbPath = path.join(process.cwd(), 'dev.db')
+const adapter = new PrismaBetterSqlite3({ url: dbPath })
 
 export const prisma =
     globalForPrisma.prisma ||
